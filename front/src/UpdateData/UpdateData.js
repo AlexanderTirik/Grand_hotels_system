5771e2f95d322f755a012e2174d306b44f4a84fe
AlexanderTirik <sasha.tirik@gmail.com>
2020-02-15 20:17:01 +0200

    Added UpdateData component

--- front/src/UpdateData/UpdateData.js
+++ front/src/UpdateData/UpdateData.js
@@ -3,26 +3,27 @@ import { useState } from "react"
 import "bootstrap/dist/css/bootstrap.css"
 
 export default function UpdateData(props) {
-  const [query, setQuery] = useState({});
+  const [query, setQuery] = useState({})
 
   const submitClick = () => {
-    // const options = {
-    //   method: "POST",
-    //   headers: new Headers({
-    //     "content-type": "application/json"
-    //   }),
-    //   body: JSON.stringify({
-    //     table: props.table,
-    //     info: query
-    //   })
-    // }
-    // ;(async () => {
-    //   const response = await fetch("http://127.0.0.1:3001/insertdata", options)
-    // })()
+    const options = {
+      method: "POST",
+      headers: new Headers({
+        "content-type": "application/json"
+      }),
+      body: JSON.stringify({
+        table: props.table,
+        info: query,
+        id: query.id
+      })
+    }
+    ;(async () => {
+      const response = await fetch("http://127.0.0.1:3001/updatedata", options)
+    })()
   }
 
-  const handleChange = (event) => {
-    const q = query;
+  const handleChange = event => {
+    const q = query
     q[event.currentTarget.name] = event.target.value
     setQuery(q)
   }
@@ -32,10 +33,19 @@ export default function UpdateData(props) {
       <form>
         {props.headers.map((hed, i) => {
           return Object.values(hed).map(header => {
-            return <input key={`${i}inp`} type="text" name={header} placeholder={header} value = {query[header]} onChange = {handleChange}/>
+            return (
+              <input
+                key={`${i}inp`}
+                type="text"
+                name={header}
+                placeholder={header}
+                value={query[header]}
+                onChange={handleChange}
+              />
+            )
           })
         })}
-        <button onClick = {submitClick}>Submit</button>
+        <button onClick={submitClick}>Submit</button>
       </form>
     </React.Fragment>
   )
