18c8c757c68ade4617fa5c7b4c52e55b9071184d
AlexanderTirik <sasha.tirik@gmail.com>
2020-02-20 18:42:26 +0200

    Edited some errors.

--- Server/server.js
+++ Server/server.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const cors = require("cors")
+const { check, validationResult } = require("express-validator")
 const app = express()
 
 app.listen(3001, () => console.log("Server ready"))
@@ -11,37 +12,37 @@ app.options("*", cors())
 app.use(express.json())
 
 app.post("/insertdata", cors(), (req, res) => {
-  table = req.body.table
-  info = req.body.info
+  const table = req.body.table
+  const info = req.body.info
   dataBase.insertData(table, info)
 })
 
 app.post("/showdata", cors(), (req, res) => {
-  table = req.body.table
+  const table = req.body.table
   dataBase.showData(table, res)
 })
 
 app.post("/showdatacolumns", cors(), (req, res) => {
-  table = req.body.table
+  const table = req.body.table
   dataBase.showDataColumns(table, res)
 })
 
 app.post("/checkdata", cors(), (req, res) => {
-  table = req.body.table
-  elKey = req.body.key
-  value = req.body[elKey]
+  const table = req.body.table
+  const elKey = req.body.key
+  const value = req.body[elKey]
   dataBase.checkData(table, elKey, value, res)
 })
 
 app.post("/deletedata", cors(), (req, res) => {
-  table = req.body.table
-  id = req.body.info.id
+  const table = req.body.table
+  const id = req.body.info.id
   dataBase.deleteData(table, id)
 })
 
 app.post("/updatedata", cors(), (req, res) => {
-  table = req.body.table
-  query = req.body.info
-  id = query.id
+  const table = req.body.table
+  const query = req.body.info
+  const id = query.id
   dataBase.updateData(table, query, id)
 })
