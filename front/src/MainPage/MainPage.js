2e4266a7a9ba9a0d183158f3c638ce23e828152e
AlexanderTirik <sasha.tirik@gmail.com>
2020-02-25 21:49:36 +0200

    Changed MainPage structure

--- front/src/MainPage/MainPage.js
+++ front/src/MainPage/MainPage.js
@@ -4,14 +4,23 @@ import "bootstrap/dist/css/bootstrap.css"
 import Greeting from "../Greeting/Greeting.js"
 import styled from "styled-components"
 import Diagrams from "../Diagrams/Diagrams.js"
+import HotelCarousel from "../HotelCarousel/HotelCarousel.js"
+
 
 
 export default function MainPage(props) {
 
+  const Div = styled.div`
+  background-color:#F6E1E8;
+  `
+
   return (
-    <React.Fragment>
+    <Div>
         <Greeting/>
         <Diagrams/>
-    </React.Fragment>
+        <Div>
+         <HotelCarousel/>
+        </Div>
+    </Div>
   )
 }
