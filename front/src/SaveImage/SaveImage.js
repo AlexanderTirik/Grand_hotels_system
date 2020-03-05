d1f6e4633a967a2da7bd959c939a0273a37728a0
AlexanderTirik <sasha.tirik@gmail.com>
2020-03-05 20:19:18 +0200

    New component to save images

---
+++ front/src/SaveImage/SaveImage.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { useState } from "react"
+import "bootstrap/dist/css/bootstrap.css"
+
+export default function SaveImage(props) {
+  const [image, setImage] = useState()
+
+  const submitClick = () => {
+    const options = {
+      method: "POST",
+      body: image
+    }
+    ;(async () => {
+      const response = await fetch("http://127.0.0.1:3001/saveimage", options)
+    })()
+  }
+  
+  const handleFile = event => {
+    const img = event.target.files[0]
+    const formData = new FormData()
+    formData.append("picture", img)
+    setImage(formData)
+  }
+
+  
+
+  return (
+    <div>
+      <form>
+        <input type="file" onChange={handleFile} />
+        <button onClick={submitClick}>Submit</button>
+      </form>
+    </div>
+  )
+}
