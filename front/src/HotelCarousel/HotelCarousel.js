b03a65232f716569d400e096d7119f51ed13a9fd
AlexanderTirik <sasha.tirik@gmail.com>
2020-03-08 19:24:27 +0200

    corrected img size on carousel

--- front/src/HotelCarousel/HotelCarousel.js
+++ front/src/HotelCarousel/HotelCarousel.js
@@ -7,7 +7,7 @@ import "../../node_modules/aos/dist/aos.css"
 import bud from "./HotelImages/Budapest.jpg"
 
 export default function HotelCarousel(props) {
-  const [data, setData] = useState();
+  const [data, setData] = useState([])
 
   const Div = styled.div`
     position: relative;
@@ -18,66 +18,45 @@ export default function HotelCarousel(props) {
     border: 4px solid #f6e1e8;
     margin: 25px 0px;
   `
-  
-  useEffect(() => {
-      const options = {
-        method: "POST",
-        headers: new Headers({
-          "content-type": "application/json"
-        }),
-        body: JSON.stringify({
-          table: "hotels"
-        })
-      }
-      ;(async () => {
-        const response = await fetch("http://127.0.0.1:3001/showdata", options)
-        const data = await response.json()
-        setData(data)
-        console.log(data);
-      })()
-  },[])
+  const Img = styled.img`
+  height:35vw;
+  width:100%;
+  `
 
+  useEffect(() => {
+    const options = {
+      method: "POST",
+      headers: new Headers({
+        "content-type": "application/json"
+      }),
+      body: JSON.stringify({
+        table: "hotels"
+      })
+    }
+    ;(async () => {
+      const response = await fetch("http://127.0.0.1:3001/showdata", options)
+      const data = await response.json()
+      setData(data)
+      console.log(data)
+    })()
+  }, [])
 
   return (
     <Div data-aos="zoom-in">
       <Carousel>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://mars-metcdn-com.global.ssl.fastly.net/content/uploads/sites/112/2017/08/06112230/800x400.png"
-            alt="Third slide"
-          />
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://mars-metcdn-com.global.ssl.fastly.net/content/uploads/sites/112/2017/08/06112230/800x400.png"
-            alt="Second slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Second slide label</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-          </Carousel.Caption>
-        </Carousel.Item>
-        <Carousel.Item>
-          <img
-            className="d-block w-100"
-            src="https://mars-metcdn-com.global.ssl.fastly.net/content/uploads/sites/112/2017/08/06112230/800x400.png"
-            alt="Third slide"
-          />
-
-          <Carousel.Caption>
-            <h3>Third slide label</h3>
-            <p>
-              Praesent commodo cursus magna, vel scelerisque nisl consectetur.
-            </p>
-          </Carousel.Caption>
-        </Carousel.Item>
+        {data.map(hot => (
+          <Carousel.Item>
+            <img
+              className="d-block w-100"
+              src="https://file.videopolis.com/F/1/9fdaf9bf-b97b-43fe-b491-b63ee0fcb3bd/101036.11808.estes-park.stanley-hotel.hero-LbqKaRfl-44080-1280x720.jpeg"
+              alt="Third slide"
+            />
+            <Carousel.Caption>
+              <h3>{hot.hotel_name}</h3>
+              <p>{hot.stars} Stars</p>
+            </Carousel.Caption>
+          </Carousel.Item>
+        ))}
       </Carousel>
     </Div>
   )
