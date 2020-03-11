70889a4692309986c6c0c444d9dad3af4fe82ae7
AlexanderTirik <sasha.tirik@gmail.com>
2020-03-11 21:04:50 +0200

    Added Carousel

--- front/src/HotelCarousel/HotelCarousel.js
+++ front/src/HotelCarousel/HotelCarousel.js
@@ -4,7 +4,6 @@ import AOS from "aos"
 import "bootstrap/dist/css/bootstrap.css"
 import { Carousel } from "react-bootstrap"
 import "../../node_modules/aos/dist/aos.css"
-import bud from "./HotelImages/Budapest.jpg"
 
 export default function HotelCarousel(props) {
   const [data, setData] = useState([])
@@ -19,8 +18,8 @@ export default function HotelCarousel(props) {
     margin: 25px 0px;
   `
   const Img = styled.img`
-  height:35vw;
-  width:100%;
+    height: 35vw;
+    width: 100%;
   `
 
   useEffect(() => {
@@ -37,27 +36,27 @@ export default function HotelCarousel(props) {
       const response = await fetch("http://127.0.0.1:3001/showdata", options)
       const data = await response.json()
       setData(data)
-      console.log(data)
     })()
   }, [])
 
+  let carousel = []
+  
+  for (const hot of data) {
+    const hotelName = hot.hotel_name;
+    const source = `http://127.0.0.1:3001/getimage/hotels/${hotelName.replace(" ", "%20")}`
+    carousel.push(
+      <Carousel.Item>
+        <img className="d block w-100" src={source} />
+        <Carousel.Caption>
+          <h3>{hot.hotel_name}</h3>
+          <p>{hot.stars} Stars</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    )
+  }
   return (
     <Div data-aos="zoom-in">
-      <Carousel>
-        {data.map(hot => (
-          <Carousel.Item>
-            <img
-              className="d-block w-100"
-              src="https://file.videopolis.com/F/1/9fdaf9bf-b97b-43fe-b491-b63ee0fcb3bd/101036.11808.estes-park.stanley-hotel.hero-LbqKaRfl-44080-1280x720.jpeg"
-              alt="Third slide"
-            />
-            <Carousel.Caption>
-              <h3>{hot.hotel_name}</h3>
-              <p>{hot.stars} Stars</p>
-            </Carousel.Caption>
-          </Carousel.Item>
-        ))}
-      </Carousel>
+      <Carousel>{carousel}</Carousel>
     </Div>
   )
 }
