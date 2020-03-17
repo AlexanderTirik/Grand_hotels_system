f2556cc24c879ece721e1de0f3e343e883a150f8
AlexanderTirik <sasha.tirik@gmail.com>
2020-03-17 18:23:46 +0200

    Added states for SignUp component and deleted DBbutton

--- front/src/MainPage/MainPage.js
+++ front/src/MainPage/MainPage.js
@@ -8,35 +8,25 @@ import HotelCarousel from "../HotelCarousel/HotelCarousel.js"
 import NavBar from "../NavBar/NavBar.js"
 import AboutUs from "../AboutUs/AboutUs"
 import Login from "../Login/Login"
+import SignUp from "../SignUp/SignUp"
+import "animate.css"
 
 export default function MainPage(props) {
-  const [isLoginOpen, setIsLoginOpen] = useState(true)
+  const [isLoginOpen, setIsLoginOpen] = useState(false)
+  const [isSignUpOpen, setIsSignUpOpen] = useState(false)
   const Div = styled.div`
     background-color: #f6e1e8;
   `
 
-  const DBButton = styled.button`
-    position: fixed;
-    padding: 10px;
-    margin: 5px;
-    background-color: rgba(244, 215, 250, 0.5);
-    border: 1px solid #f6e1e8;
-    border-radius: 10px;
-    font-family: Futura, "Trebuchet MS", Arial, sans-serif;
-    color: #754857;
-    font-size: 1.5em;
-    z-index: 999;
-    left: 0%;
-    top: 0%;
-  `
-
+  let NavBarComp;
+  if (!(isLoginOpen || isSignUpOpen)){
+    NavBarComp = (<NavBar setIsLoginOpen = {setIsLoginOpen} setIsSignUpOpen = {setIsSignUpOpen}/>)
+  }
   return (
     <Div>
-      <Login isLoginOpen={isLoginOpen} setIsLoginOpen = {setIsLoginOpen}/>
-      {/* <Link to={`/dbcontrol`}>
-        <DBButton>Edit DataBase</DBButton>
-      </Link> */}
-      <NavBar setIsLoginOpen = {setIsLoginOpen}/>
+      <Login isLoginOpen={isLoginOpen} setIsLoginOpen = {setIsLoginOpen} setIsSignUpOpen = {setIsSignUpOpen}/>
+      <SignUp isSignUpOpen={isSignUpOpen} setIsSignUpOpen = {setIsSignUpOpen} setIsLoginOpen = {setIsLoginOpen}/>
+      {NavBarComp}
       <Greeting />
       <Diagrams />
       <HotelCarousel />
