384a9433f455c4ebd8b539a08167fe78f6ee75f9
AlexanderTirik <sasha.tirik@gmail.com>
2020-03-29 13:25:54 +0300

    Finished SignUp component

--- front/src/SignUp/SignUp.js
+++ front/src/SignUp/SignUp.js
@@ -9,10 +9,9 @@ Modal.setAppElement("#root")
 export default function SignUp(props) {
   const ModalWindow = styled(Modal)`
     position: absolute;
-    top: 20%;
+    top: 10%;
     left: 35%;
     right: 35%;
-    bottom: 20%;
     background-image: linear-gradient(#f6e1e8, white);
     border: 3px dashed #754857;
     border-radius: 5%;
@@ -33,7 +32,7 @@ export default function SignUp(props) {
     font-family: Futura, "Trebuchet MS", Arial, sans-serif;
   `
   const WordSignUp = styled.div`
-    font-size: 2.5vw;
+    font-size: 2.4vw;
     font-family: Futura, "Trebuchet MS", Arial, sans-serif;
     color: #754857;
     font-weight: bolder;
@@ -45,27 +44,6 @@ export default function SignUp(props) {
     font-family: Futura, "Trebuchet MS", Arial, sans-serif;
     font-size: 1.5vw;
   `
-
-  // const submitClick = () => {
-  //   if (user.confirmPassword === user.password) {
-  //     const options = {
-  //       method: "POST",
-  //       headers: new Headers({
-  //         "content-type": "application/json"
-  //       }),
-  //       body: JSON.stringify({
-  //         first_name: user.firstName,
-  //         second_name: user.secondName,
-  //         email: user.email,
-  //         password: user.password
-  //       })
-  //     }
-  //     ;(async () => {
-  //       const response = await fetch("http://127.0.0.1:3001/newUser", options)
-  //     })()
-  //   }
-  // }
-
   return (
     <ModalWindow
       isOpen={props.isSignUpOpen}
