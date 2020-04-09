99896597c1fe25c9ccc71e56c65806b1fd53e61d
AlexanderTirik <sasha.tirik@gmail.com>
2020-04-09 16:59:15 +0300

    Added standart image

--- Server/controllers/getImage.js
+++ Server/controllers/getImage.js
@@ -1,8 +1,23 @@
 const getImage = async (req, res) => {
+  const fs = require("fs")
   const file =
-  process.cwd()+"/public/images/" + `${req.params.type}/` + `${req.params.name}.jpg`
+    process.cwd() +
+    "/public/images/" +
+    `${req.params.type}/` +
+    `${req.params.name}.jpg`
 
-  res.sendFile(file)
+  fs.access(file, fs.F_OK, err => {
+    if (err) {
+      const standart =
+        process.cwd() +
+        "/public/images/" +
+        `${req.params.type}/` +
+        `standart.jpg`
+      res.sendFile(standart)
+      return
+    }
+    res.sendFile(file)
+  })
 }
 
 exports.getImage = getImage
